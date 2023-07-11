9c217046599fc22319903b6cacb19fc4295feb35
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-07-11 20:20:00 +0530

    submitting code-Tue Jul 11 2023 20:19:58 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
 
      return(
       <div>
-           <p>Button clicked {count} times</p>
+           <p>Button clicked {count}</p>
            <button onClick={()=>setCount(count+1)}>Click Me</button>
       </div>
      )
